c800da0ffb2a3202e7bd15fb990f976f1dbc2d34
bokuweb <bokuweb12@gmail.com>
2016-08-10 11:40:20 +0900

    Update example

--- example/src/example.js
+++ example/src/example.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react';
-import HelloWorld from '../../src';
-
+import React from 'react';
+import Table from '../../src';
 
 const data = [
-  { id: 3, name: 'Satoshi Yamamoto', class: 'B' },
-  { id: '2012/1/2', name: 'Taro Tanaka', class: 'A', props: {className: 'hoge' }},
-  { id: '2013/01/01', name: 'Ken Asada', class: 'A',  },
-  { id: '2012/01/01', name: 'Masaru Tokunaga', class: 'C'  },
+  { id: 0, name: 'Robert Mikels', createdAt: '2016/1/2' },
+  { id: 1, name: 'Karyn Holmberg', props: { className: 'custom-class' }, createdAt: '2015/10/21' },
+  { id: 2, name: 'John Smith', createdAt: '2012/5/2' },
+  { id: 3, name: 'Misty Abbott', createdAt: '2016/8/1' },
 ];
 
 const columns = [
   {
-    columnStyle: { width: '30px' },
+    columnStyle: { width: '50px' },
     name: 'ID',
     key: 'id',
     props: { className: 'test' },
-    // render: (id) => { return <a href={'user/'+id}>{id}</a>; }
-  },
-  {
-    name: 'NAME',
+  }, {
+    name: 'Name',
     key: 'name',
-    props: { className: 'align-left' },
+    props: { className: 'custom-head-class' },
+  }, {
+    name: 'Create at',
+    key: 'createdAt',
   },
-  {
-    name: 'CLASS',
-    key: 'class',
-    sortable: false,
-  }
 ];
 
 
-export default class Example extends Component{
-  render() {
-    return (
-      <HelloWorld
-        data={data}
-        columns={columns}
-        isBordered
-        isStriped
-        defaultSort={{ key: 'name', type: 'desc' }}
-      />
-    );
-  }
+export default function Example() {
+  return (
+    <Table
+      data={data}
+      columns={columns}
+      isStriped
+      styles={{
+        icon: { top: '3px' },
+        table: { borderTop: '2px solid #ccc' },
+      }}
+      defaultSort={{ key: 'name', type: 'desc' }}
+    />
+  );
 }
